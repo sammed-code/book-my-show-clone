085a54c315bae5c544dcd1469d5279e76fed4517
sam <83267739+sammed-code@users.noreply.github.com>
2024-01-27 10:37:52 +0000

    3

--- src/layout/Default.layout.jsx
+++ src/layout/Default.layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 // Component
-import Navbar from "../components/Navbar.Component";
+import Navbar from "../components/Navbar/Navbar.Component";
 
 const DefaultlayoutHoc =
   (Component) =>
